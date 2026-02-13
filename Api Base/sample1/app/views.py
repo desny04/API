d612094748a0553db6c7f9a910217436d9c6b66e
desny <trsadesny@gmail.com>
2026-02-13 11:16:51 +0530

    put & delete

--- Api Base/sample1/app/views.py
+++ Api Base/sample1/app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse
 from .models import*
 from .serializers import*
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
@@ -16,20 +19,80 @@ from rest_framework.parsers import JSONParser
 #         s=StudentSerializer(d,many=True)
 #         return JsonResponse(s.data,safe=False)
 
-@csrf_exempt
-def fun(request):
+# @csrf_exempt
+# def fun(request):
+#     if request.method=='GET':
+#         d=Students.objects.all()
+#         s=StudentModelSerializer(d,many=True)
+#         return JsonResponse(s.data,safe=False)
+#     elif request.method=='POST':
+#         d=JSONParser().parse(request)
+#         s=StudentModelSerializer(data=d)
+#         if s.is_valid():
+#             s.save()
+#             return JsonResponse(s.data)
+#         else:
+#             return JsonResponse(s.error)
+
+# @csrf_exempt
+# def fun(request,d):
+#     try:
+#         demo=Students.objects.get(pk=d)
+#     except:
+#         return HttpResponse('Student not found')
+
+#     if request.method=='GET':
+#         s=StudentModelSerializer(demo)
+#         return JsonResponse(s.data,safe=False)
+#     elif request.method=='PUT':
+#         d=JSONParser().parse(request)
+#         s=StudentModelSerializer(demo,data=d)
+#         if s.is_valid():
+#             s.save()
+#             return JsonResponse(s.data)
+#         else:
+#             return JsonResponse(s.errors)
+#     elif request.method=='DELETE':
+#         demo.delete()
+#         return HttpResponse('Deleted')
+
+
+# @api_view(['GET','POST'])
+# def fun(request):
+#     if request.method=='GET':
+#         d=Students.objects.all()
+#         s=StudentModelSerializer(d,many=True)
+#         return Response(s.data)
+#     elif request.method=='POST':
+#         d=JSONParser().parse(request)
+#         s=StudentModelSerializer(data=d)
+#         if s.is_valid():
+#             s.save()
+#             return JsonResponse(s.data,status=status.HTTP_201_CREATED)
+#         else:
+#             return JsonResponse(s.error,status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET','POST','PUT','DELETE'])
+def fun(request,d):
+    try:
+        demo=Students.objects.get(pk=d)
+    except:
+        return HttpResponse('Student not found')
+
     if request.method=='GET':
-        d=Students.objects.all()
-        s=StudentModelSerializer(d,many=True)
+        s=StudentModelSerializer(demo)
         return JsonResponse(s.data,safe=False)
-    elif request.method=='POST':
+    elif request.method=='PUT':
         d=JSONParser().parse(request)
-        s=StudentModelSerializer(data=d)
+        s=StudentModelSerializer(demo,data=d)
         if s.is_valid():
             s.save()
-            return JsonResponse(s.data)
+            return JsonResponse(s.data,status=status.HTTP_201_CREATED)
         else:
-            return JsonResponse(s.error)
-
+            return JsonResponse(s.errors,status=status.HTTP_400_BAD_REQUEST)
+    elif request.method=='DELETE':
+        demo.delete()
+        return HttpResponse('Deleted')
 
 
--- Api Base/sample1/sample1/urls.py
+++ Api Base/sample1/sample1/urls.py
@@ -20,5 +20,6 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.fun),
+    path('fun/<d>',views.fun),
+    path('fun/',views.fun),
 ]
