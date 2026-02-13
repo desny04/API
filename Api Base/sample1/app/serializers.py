f62acd43814bfc821fdf5ad83816e9508705a2f1
desny <trsadesny@gmail.com>
2026-02-13 09:39:43 +0530

    get & push

--- Api Base/sample1/app/serializers.py
+++ Api Base/sample1/app/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
+from .models import*
 
-class StudentSerializer(serializers.Serializer):
-    roll =serializers.IntegerField()
-    name=serializers.CharField()
-    place=serializers.CharField()
+# class StudentSerializer(serializers.Serializer):
+#     roll =serializers.IntegerField()
+#     name=serializers.CharField()
+#     place=serializers.CharField()
+
+class StudentModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Students
+        fields='__all__'
--- Api Base/sample1/app/views.py
+++ Api Base/sample1/app/views.py
@@ -2,14 +2,34 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import*
 from .serializers import*
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
 
 # def fun(request):
 #     return JsonResponse({'Name':'Desny','Age': 21,'Address': 'Kochi'})
 
+# def fun(request):
+#     if request.method=='GET':
+#         d=Students.objects.all()
+#         s=StudentSerializer(d,many=True)
+#         return JsonResponse(s.data,safe=False)
+
+@csrf_exempt
 def fun(request):
     if request.method=='GET':
         d=Students.objects.all()
-        s=StudentSerializer(d,many=True)
+        s=StudentModelSerializer(d,many=True)
         return JsonResponse(s.data,safe=False)
+    elif request.method=='POST':
+        d=JSONParser().parse(request)
+        s=StudentModelSerializer(data=d)
+        if s.is_valid():
+            s.save()
+            return JsonResponse(s.data)
+        else:
+            return JsonResponse(s.error)
+
+
+
